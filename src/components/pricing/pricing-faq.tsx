80fe9118d50048f30a9014005567e23b8f823cb3
Ravindu Gunawardana <ravindugunawardana7@gmail.com>
2026-05-20 10:18:03 +0530

    Refactor Contact Us button for improved readability and styling consistency

--- src/components/pricing/pricing-faq.tsx
+++ src/components/pricing/pricing-faq.tsx
@@ -136,10 +136,12 @@ export default function FAQSection() {
             <p className="mb-3 body-small text-[#01030B">
               My question is not here.
             </p>
-            <Link href="/contact-us">
-              <button className="body-medium rounded bg-animated-gradient px-6 py-2 text-white">
-                Contact Us
-              </button>
+
+            <Link
+              href="/contact-us"
+              className="body-medium rounded bg-animated-gradient px-6 py-2 text-white"
+            >
+              Contact Us
             </Link>
           </div>
         </div>
